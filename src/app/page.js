28e21ac3620193f97a691f3ec4a94dc50d072007
Filip Jerga <jerga99@gmail.com>
2023-10-05 11:58:28 +0200

    Display Blogs and Portfolios

--- src/app/page.js
+++ src/app/page.js
@@ -2,31 +2,25 @@
 import blogs from "@/content/blogs.json";
 import portfolios from "@/content/portfolios.json";
 
-const items2 = [
-  {value: 100, prop: "prop1"}, 
-  {value: "Hi There",  prop: "prop2"}, 
-  {value: 10,  prop: "prop3"}, 
-  {value: "Something",  prop: "prop4"}
-];
 
 export default function Home() {
-
-  const displayItems = () => 
-    items2.map(item => 
-      <div key={item.value}>{item.value} - {item.prop}</div>
-    );
-  
 
   return (
     <>
       <div>Hello World</div>
       <div> 
-        { displayItems() }
-
-        <hr></hr>
-        { items2.map(obj =>
-          <div key={obj.value}>{obj.value} - {obj.prop}</div>
-        )}
+        <div>Blogs</div>
+        <div>
+          { blogs.map(blog =>
+            <div key={blog.id}>{blog.title}</div>
+          )}
+        </div>
+        <div>Portfolios</div>
+        <div>
+          { portfolios.map(portfolio =>
+            <div key={portfolio.id}>{portfolio.title}</div>
+          )}
+        </div>
       </div>
     </>
   )
