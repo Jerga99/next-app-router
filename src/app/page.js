5f772e7f58e8a935f4ed407d570d8a3d53424013
Filip Jerga <jerga99@gmail.com>
2023-10-05 11:41:39 +0200

    Map function

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,25 @@
 import blogs from "@/content/blogs.json";
 import portfolios from "@/content/portfolios.json";
 
+const items2 = [
+  {value: 100, prop: "prop1"}, 
+  {value: "Hi There",  prop: "prop2"}, 
+  {value: 10,  prop: "prop3"}, 
+  {value: "Something",  prop: "prop4"}
+];
+
 export default function Home() {
 
   return (
     <>
       <div>Hello World</div>
-      <div>
-        {JSON.stringify(blogs)}
-        {JSON.stringify(portfolios)}
+      <div> 
+        { items2.map((item) => {
+          console.log(item);
+          return (
+            <div key={item.value}>{item.value} - {item.prop}</div>
+          )
+        })}
       </div>
     </>
   )
