dce599d6a11a77485d21fee64a0949a3dc3b0833
Filip Jerga <jerga99@gmail.com>
2023-10-05 12:08:48 +0200

    Display Images

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 
 import blogs from "@/content/blogs.json";
 import portfolios from "@/content/portfolios.json";
+import Image from "next/image";
 
 
 export default function Home() {
@@ -12,13 +13,31 @@ export default function Home() {
         <div>Blogs</div>
         <div>
           { blogs.map(blog =>
-            <div key={blog.id}>{blog.title}</div>
+            <div key={blog.id}>
+              <Image 
+                src={blog.coverImage} 
+                width={100} 
+                height={100} 
+                alt={blog.title} 
+              />
+              <div>{blog.title}</div>
+              <div>{blog.description}</div>
+            </div>
           )}
         </div>
         <div>Portfolios</div>
         <div>
           { portfolios.map(portfolio =>
-            <div key={portfolio.id}>{portfolio.title}</div>
+            <div key={portfolio.id}>
+              <Image 
+                src={portfolio.coverImage} 
+                width={100} 
+                height={100} 
+                alt={portfolio.title} 
+              />
+              <div>{portfolio.title}</div>
+              <div>{portfolio.description}</div>
+            </div>
           )}
         </div>
       </div>
