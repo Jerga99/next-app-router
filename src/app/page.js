0916fa3fd44f0e5289d4f54dadb2b2c1b4c3bcdf
Filip Jerga <jerga99@gmail.com>
2023-10-07 18:43:59 +0200

    Fetching data with API route

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,22 @@
 import { BlogList } from "@/components/blogs/BlogList";
 import { PortfolioList } from "@/components/portfolios/PortfolioList";
 
-import blogs from "@/content/blogs.json";
 import portfolios from "@/content/portfolios.json";
 
-export default function Home() {
+async function getBlogs() {
+  console.log("Fetching Blogs");
+  const res = await fetch("http://localhost:3000/api/blogs");
+  console.log("Getting Blogs");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data!");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const {data: blogs} = await getBlogs();
+
   return (
     <>
       <BlogList blogs={blogs} />
