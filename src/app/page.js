c41148254af2e64e392c05c41f1eb236035ebeac
Filip Jerga <jerga99@gmail.com>
2023-10-17 10:00:27 +0100

    Server side vs static

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,25 @@
 
 import { BlogList } from "@/components/blogs/BlogList";
 import { PortfolioList } from "@/components/portfolios/PortfolioList";
+import { delay } from "@/utils";
 
 async function getBlogs() {
+  console.log("Fetching Blogs");
+  const random = Math.random();
+  await delay(500);
   const res = await fetch("http://localhost:3001/api/blogs", {cache: "no-cache"});
   if (!res.ok) {
     throw new Error("Failed to fetch data!");
   }
 
-  return res.json();
+  const blogs = await res.json();
+
+  return {blogs, random};
 }
 
 async function getPortfolios() {
+  console.log("Fetching Portfolios");
+  await delay(1000);
   const res = await fetch("http://localhost:3001/api/portfolios", {cache: "no-cache"});
 
   if (!res.ok) {
@@ -22,12 +30,13 @@ async function getPortfolios() {
 }
 
 export default async function Home() {
-  const {data: blogs} = await getBlogs();
+  const {blogs, random} = await getBlogs();
   const {data: portfolios} = await getPortfolios();
 
   return (
     <>
-      <BlogList blogs={blogs} />
+      <h1>{random}</h1>
+      <BlogList blogs={blogs.data} />
       <PortfolioList portfolios={portfolios} />
     </>
   )
