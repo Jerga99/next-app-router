851cea0a4480f18e70080d0a1aa15411394e19bb
Filip Jerga <jerga99@gmail.com>
2023-10-18 07:57:01 +0100

    Read files on Server

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
 
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
 const port = 3001;
 
-const portfolios = require("./content/portfolios.json");
-const blogs = require("./content/blogs.json");
+const pathToContent = path.join(__dirname, "content");
 
 app.get("/", (req, res) => {
   res.json({data: "Hello Node JS Server!"});
 });
 
 app.get("/api/blogs", (req, res) => {
-  res.json({data: blogs});
+  const blogsPath = path.join(pathToContent , "blogs.json");
+  const blogs = fs.readFileSync(blogsPath, "utf-8");
+  res.json({data: JSON.parse(blogs)});
 });
 
 app.get("/api/portfolios", (req, res) => {
-  res.json({data: portfolios});
+  const portfolioPath = path.join(pathToContent , "portfolios.json");
+  const portfolios = fs.readFileSync(portfolioPath, "utf-8");
+  res.json({data: JSON.parse(portfolios)});
 })
 
 app.listen(port, () => {
