d4899b59befedcbcc9110cac6146bd177d1a0302
Filip Jerga <jerga99@gmail.com>
2023-10-18 09:46:43 +0100

    Tag Revalidation Finish

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,8 +1,21 @@
 
+import { revalidateTag } from "next/cache";
 
 // http://localhost:3000/api/revalidate?tag=blogs&secret=asd098as90d8a
 
 export async function POST(request) {
+  const tag = request.nextUrl.searchParams.get("tag");
+  const secret = request.nextUrl.searchParams.get("secret");
 
-  return Response.json({data: "Hello Revalidate Endpoint!"});
+  if (secret != process.env.REVALIDATION_TOKEN) {
+    return Response.json({message: "Invalid Secret!"}, {status: 401});
+  }
+
+  if (!tag) {
+    return Response.json({message: "Missing tag param!"}, {status: 400});
+  }
+
+  revalidateTag(tag)
+
+  return Response.json({revalidated: true, now: Date.now()});
 }
