b6f57be338427a2c04db3709f54af1af6edf82cd
Filip Jerga <jerga99@gmail.com>
2023-10-18 10:00:02 +0100

    Path Revalidation

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,21 +1,44 @@
 
-import { revalidateTag } from "next/cache";
+import { revalidateTag, revalidatePath } from "next/cache";
 
-// http://localhost:3000/api/revalidate?tag=blogs&secret=asd098as90d8a
+// --- TAG REVALIDATION ---
+
+// http://localhost:3000/api/revalidate?tag=blogs&secret=HelloWorld123
+
+// export async function POST(request) {
+//   const tag = request.nextUrl.searchParams.get("tag");
+//   const secret = request.nextUrl.searchParams.get("secret");
+
+//   if (secret != process.env.REVALIDATION_TOKEN) {
+//     return Response.json({message: "Invalid Secret!"}, {status: 401});
+//   }
+
+//   if (!tag) {
+//     return Response.json({message: "Missing tag param!"}, {status: 400});
+//   }
+
+//   revalidateTag(tag)
+
+//   return Response.json({revalidated: true, now: Date.now()});
+// }
+
+// --- PATH REVALIDATION ---
+
+// http://localhost:3000/api/revalidate?path=/&secret=HelloWorld123
 
 export async function POST(request) {
-  const tag = request.nextUrl.searchParams.get("tag");
+  const path = request.nextUrl.searchParams.get("path");
   const secret = request.nextUrl.searchParams.get("secret");
 
   if (secret != process.env.REVALIDATION_TOKEN) {
     return Response.json({message: "Invalid Secret!"}, {status: 401});
   }
 
-  if (!tag) {
-    return Response.json({message: "Missing tag param!"}, {status: 400});
+  if (!path) {
+    return Response.json({message: "Missing path param!"}, {status: 400});
   }
 
-  revalidateTag(tag)
+  revalidatePath(path)
 
   return Response.json({revalidated: true, now: Date.now()});
 }
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,7 +1,7 @@
 
 
 export async function getBlogs() {
-  const res = await fetch("http://localhost:3001/api/blogs", {next: {tags: ["blogs"]}});
+  const res = await fetch("http://localhost:3001/api/blogs");
 
   if (!res.ok) {
     throw new Error("Failed to fetch data!");
