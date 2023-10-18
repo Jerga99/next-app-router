ad43581f12f2e2deead2c29f85d18c64e3c04eb9
Filip Jerga <jerga99@gmail.com>
2023-10-18 17:41:02 +0100

    Detail pages

---
+++ src/app/blogs/[slug]/page.js
@@ -0,0 +1,11 @@
+
+
+
+export default function BlogDetail({params}) {
+
+  return (
+    <>
+      Blog Detail of: {params.slug}
+    </>
+  )
+}
---
+++ src/app/portfolios/[slug]/page.js
@@ -0,0 +1,13 @@
+
+
+
+
+export default function PortfolioDetail({params}) {
+
+  return (
+    <>
+      Portfolio Detail of: {params.slug}
+    </>
+  )
+}
+
