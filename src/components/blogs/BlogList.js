639b8c75143b2d91b4bd1deefb024cc96b20b80c
Filip Jerga <jerga99@gmail.com>
2023-10-19 13:21:08 +0100

    Fix images and Titles

--- src/components/blogs/BlogList.js
+++ src/components/blogs/BlogList.js
@@ -1,48 +1,59 @@
 
 import { getBlogs } from "@/utils";
+import Image from "next/image";
 import Link from "next/link";
 
 export async function BlogList() {
   const blogs = getBlogs();
 
   return (
-    <div className="mb-14">
-      <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
-        {blogs.map((blog) => (
-          <div
-            key={blog.id}
-            data-tip
-            data-for={`course-${blog.slug}`}
-            className="group relative bg-white border border-gray-200 rounded-lg flex flex-col overflow-hidden"
-          >
-            <div className="bg-gray-200 group-hover:opacity-75 sm:aspect-none">
-              <img
-                src={blog.coverImage}
-                alt={blog.title}
-                className="w-full h-full object-center object-cover sm:w-full sm:h-full"
-              />
-            </div>
-            <div className="flex-1 p-4 space-y-2 flex flex-col">
-              <h3 className="text-sm font-medium text-gray-900">
-                <Link href={`/courses/${blog.slug}`}>
-                  <span aria-hidden="true" className="absolute inset-0" />
-                  {blog.title}
-                </Link>
-              </h3>
-              <p className="text-sm text-gray-500">{blog.description}</p>
-              <div className="flex-1 flex flex-col justify-end">
-                <a
-                  target="_"
-                  href="/"
-                  className="text-base font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  See Details
-                </a>
+    <>
+      <div className="mb-4">
+        <h2 className="text-3xl mb-0">Blogs</h2>
+        <Link href={"/blogs"} className="underline button is-text">
+          See All
+        </Link>
+      </div>
+      <div className="mb-14">
+        <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+          {blogs.map((blog) => (
+            <div
+              key={blog.slug}
+              data-tip
+              data-for={`course-${blog.slug}`}
+              className="group relative bg-white border border-gray-200 rounded-lg flex flex-col overflow-hidden"
+            >
+              <div className="bg-gray-200 h-56 group-hover:opacity-75 sm:aspect-none relative">
+                <Image 
+                  src={blog.coverImage} 
+                  fill={true} 
+                  style={{objectFit: "cover"}}
+                  sizes="(max-width: 768px)"
+                  alt={blog.title} 
+                />
+              </div>
+              <div className="flex-1 p-4 space-y-2 flex flex-col">
+                <h3 className="text-sm font-medium text-gray-900">
+                  <Link href={`/courses/${blog.slug}`}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {blog.title}
+                  </Link>
+                </h3>
+                <p className="text-sm text-gray-500">{blog.description}</p>
+                <div className="flex-1 flex flex-col justify-end">
+                  <a
+                    target="_"
+                    href="/"
+                    className="text-base font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    See Details
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/components/portfolios/PortfolioList.js
+++ src/components/portfolios/PortfolioList.js
@@ -1,46 +1,58 @@
 import { getPortfolios } from "@/utils";
+import Image from "next/image";
 import Link from "next/link";
 
 export function PortfolioList() {
   const portfolios = getPortfolios();
   return (
-    <div className="mb-14">
-      <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
-        {portfolios.map((portfolio) => (
-          <div
-            key={portfolio.id}
-            data-tip
-            data-for={`course-${portfolio.slug}`}
-            className="group relative bg-white border border-gray-200 rounded-lg flex flex-col overflow-hidden"
-          >
-            <div className="bg-gray-200 group-hover:opacity-75 sm:aspect-none">
-              <img
-                src={portfolio.coverImage}
-                alt={portfolio.title}
-                className="w-full h-full object-center object-cover sm:w-full sm:h-full"
-              />
-            </div>
-            <div className="flex-1 p-4 space-y-2 flex flex-col">
-              <h3 className="text-sm font-medium text-gray-900">
-                <Link href={`/courses/${portfolio.slug}`}>
-                  <span aria-hidden="true" className="absolute inset-0" />
-                  {portfolio.title}
-                </Link>
-              </h3>
-              <p className="text-sm text-gray-500">{portfolio.description}</p>
-              <div className="flex-1 flex flex-col justify-end">
-                <a
-                  target="_"
-                  href="/"
-                  className="text-base font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  See Details
-                </a>
+    <>
+      <div className="mb-4">
+        <h2 className="text-3xl mb-0">Portfolios</h2>
+        <Link href={"/portfolios"} className="underline button is-text">
+          See All
+        </Link>
+      </div>
+      <div className="mb-14">
+        <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+          {portfolios.map((portfolio) => (
+            <div
+              key={portfolio.slug}
+              data-tip
+              data-for={`course-${portfolio.slug}`}
+              className="group relative bg-white border border-gray-200 rounded-lg flex flex-col overflow-hidden"
+            >
+              <div className="bg-gray-200 h-56 group-hover:opacity-75 sm:aspect-none relative">
+                <Image 
+                  src={portfolio.coverImage} 
+                  fill={true} 
+                  style={{objectFit: "cover"}}
+                  sizes="(max-width: 768px)"
+                  alt={portfolio.title} 
+                />
+              </div>
+              <div className="flex-1 p-4 space-y-2 flex flex-col">
+                <h3 className="text-sm font-medium text-gray-900">
+                  <Link href={`/courses/${portfolio.slug}`}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {portfolio.title}
+                  </Link>
+                </h3>
+                <p className="text-sm text-gray-500">{portfolio.description}</p>
+                <div className="flex-1 flex flex-col justify-end">
+                  <a
+                    target="_"
+                    href="/"
+                    className="text-base font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    See Details
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
+
